68197b7ded5ad0136e77fc2465222897fa39abad
nyguerrillagirl <nyguerrillagirl@brainycode.com>
2025-04-01 06:56:00 -0400

    Day #186 - React, Flappy, Restart Datacamp Seaborn course, Live Event

--- src/udemy_courses/flappy_bird/SFML Group/SFML Template/main.cpp
+++ src/udemy_courses/flappy_bird/SFML Group/SFML Template/main.cpp
@@ -1,35 +1,9 @@
 // SFML Template.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
-#include <SFML/Graphics.hpp>
-
-#define SCREEN_WIDTH 1024
-#define SCREEN_HEIGHT 1024
+#include <iostream>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT),
-        "Awesome SFML");
-
-    while (window.isOpen()) {
-         sf::Event event;
-        while (window.pollEvent(event)) {
-            // handle events
-            switch (event.type) {
-            case sf::Event::Closed:
-                window.close();
-                break;
-            }
-        }
-        // update game logic 
-
-        // draw
-        window.clear();
-
-        // draw SFML object
-
-        window.display();
-    }
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
 
